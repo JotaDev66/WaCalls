4aabbd78fbe61fdba7c8789bf2ea2dd3b61042cc
Joseph Fernandes <joseph.automatus@gmail.com>
2026-07-12 16:40:22 -0300

    style(call): gofmt fake relay method alignment

--- internal/voip/call/roundtrip_test.go
+++ internal/voip/call/roundtrip_test.go
@@ -63,23 +63,23 @@ func (r *fakeRelay) Broadcast(data []byte) {
 		r.onData(data)
 	}
 }
-func (r *fakeRelay) HasConnection() bool                     { return true }
-func (r *fakeRelay) SetSsrc(uint32)                          {}
-func (r *fakeRelay) SetSubscriptionSsrc(uint32)              {}
-func (r *fakeRelay) SetStreamSsrcs([]uint32, []uint32)       {}
-func (r *fakeRelay) SetOnConnected(func(string, int)) {}
-func (r *fakeRelay) SetOnReceive(func([]byte))        {}
-func (r *fakeRelay) SetOnUsableChange(func(int))      {}
-func (r *fakeRelay) ResendSubscriptions()             {}
+func (r *fakeRelay) HasConnection() bool               { return true }
+func (r *fakeRelay) SetSsrc(uint32)                    {}
+func (r *fakeRelay) SetSubscriptionSsrc(uint32)        {}
+func (r *fakeRelay) SetStreamSsrcs([]uint32, []uint32) {}
+func (r *fakeRelay) SetOnConnected(func(string, int))  {}
+func (r *fakeRelay) SetOnReceive(func([]byte))         {}
+func (r *fakeRelay) SetOnUsableChange(func(int))       {}
+func (r *fakeRelay) ResendSubscriptions()              {}
 func (r *fakeRelay) ConfigureRelays(relays []transport.RelayConfig) {
 	if r.onConfigure != nil {
 		r.onConfigure(relays)
 	}
 }
-func (r *fakeRelay) BufferedAmount() uint64                  { return 0 }
-func (r *fakeRelay) ConnectedCount() int                     { return 1 }
-func (r *fakeRelay) Cleanup()                                {}
-func (r *fakeRelay) SetObserver(core.CallObserver)           {}
+func (r *fakeRelay) BufferedAmount() uint64        { return 0 }
+func (r *fakeRelay) ConnectedCount() int           { return 1 }
+func (r *fakeRelay) Cleanup()                      {}
+func (r *fakeRelay) SetObserver(core.CallObserver) {}
 
 func km(seed byte) core.SrtpKeyingMaterial {
 	mk := make([]byte, 16)
